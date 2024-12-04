be7caff43870d13b470d8968c8c97b4230d77c27
LyneSultan <lynesultane@gmail.com>
2024-12-04 23:37:37 +0200

    added a route

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Quiz from './pages/Quiz';
 import Quizzes from './pages/Quizzes';
 import store from './redux/store';
 import { routes } from './routes/routes';
@@ -11,6 +12,7 @@ function App() {
       <Provider store={store}>
         <Routes>
           <Route path={routes.quizzes} element={<><Quizzes /></>} />
+          <Route path="/quiz/:id" element={<><Quiz /></>} />
         </Routes>
       </Provider>
     </>
