2c1eb42ae9bfd30989b8fc28ae2d1883fc10017c
LyneSultan <lynesultane@gmail.com>
2024-12-04 21:42:14 +0200

    provided the redux store

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,18 @@
+import { Provider } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Quizzes from './pages/Quizzes';
+import store from './redux/store';
+import { routes } from './routes/routes';
 
 function App() {
   return (
     <>
+      <Provider store={store}>
+        <Routes>
+          <Route path={routes.quizzes} element={<><Quizzes /></>} />
+        </Routes>
+      </Provider>
     </>
   );
 }
