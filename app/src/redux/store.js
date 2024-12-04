9c16820cd6871b33434c7e277d8a63b0faaf0e89
LyneSultan <lynesultane@gmail.com>
2024-12-04 22:58:31 +0200

    added user slice to store

--- app/src/redux/store.js
+++ app/src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import quizzesReducer from './quizzesSlice';
+import userReducer from './userSlice';
 
 export default configureStore({
   reducer: {
     quizzes: quizzesReducer,
+    user: userReducer,
+
   }
 })
