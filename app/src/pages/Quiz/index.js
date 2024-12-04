2ee1fae5d78d0083478d1d1de75838e2bc1a56d7
LyneSultan <lynesultane@gmail.com>
2024-12-04 23:49:27 +0200

    updated quiz component

--- app/src/pages/Quiz/index.js
+++ app/src/pages/Quiz/index.js
@@ -1,46 +1,12 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { incrementScore } from './../../redux/userSlice';
 
 import { useParams } from 'react-router-dom';
 import './../../style/base.css';
+import { useQuizLogic } from './useQuizLogic';
+
 const Quiz = () => {
-  const quizzes = useSelector((state) => state.quizzes);
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-  const [answers, setAnswers] = useState({});
 
   const { id } = useParams();
-  // console.log(id);
-  console.log(user);
-
-  const quiz = quizzes.find((quiz) => quiz.id === parseInt(id));
-  // console.log(quizzes);
-
-  const handleAnswerChange = (questionId, answer) => {
-    setAnswers((prevAnswers) => ({
-      ...prevAnswers,
-      [questionId]: answer,
-    }));
-  };
-
-  const handleSubmit = () => {
-    // let score = 0;
-
-    quiz.questions.forEach((question) => {
-      const userAnswer = answers[question.id];
-
-      if (
-        (question.type === "multiple-choice" && userAnswer === question.correctAnswer) ||
-        (question.type === "input" && userAnswer === question.correctAnswer)
-      ) {
-        dispatch(incrementScore());
-
-        // score++;
-      }
-    });
-
-  };
+  const { quiz, handleAnswerChange, handleSubmit } = useQuizLogic(id);
 
   return (
     <>{!quiz ? <h1>Not a valid quiz id</h1> :
