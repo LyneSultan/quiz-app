790e4687daa961006a5ca000949ca2c9791facc2
LyneSultan <lynesultane@gmail.com>
2024-12-05 10:50:48 +0200

    created quiz card component

---
+++ app/src/components/QuizCard.jsx
@@ -0,0 +1,26 @@
+import { CardContent, Typography } from '@mui/material';
+import React from 'react';
+import { ButtonComponent } from './../base/Button/Button.jsx';
+
+const QuizCard = ({ quiz, handleNavigate }) => {
+  return (
+    <>
+      <CardContent>
+        <Typography variant="h6" component="div">
+          {quiz.title}
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <ButtonComponent
+          variant="contained"
+          color="primary"
+          onClick={() => handleNavigate(quiz.id)}
+          text="Take this Quiz"
+        />
+
+      </CardContent>
+    </>
+  );
+};
+
+export default QuizCard;
