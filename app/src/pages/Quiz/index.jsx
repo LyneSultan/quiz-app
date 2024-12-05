b8b60f3fc8f74837ff42776b1fc433eb081ca6fd
LyneSultan <lynesultane@gmail.com>
2024-12-05 10:51:55 +0200

    updated quiz page

--- app/src/pages/Quiz/index.jsx
+++ app/src/pages/Quiz/index.jsx
@@ -1,58 +1,35 @@
-
-import TextField from '@mui/material/TextField';
 import { useParams } from 'react-router-dom';
 import { ButtonComponent } from '../../base/Button/Button';
-import Input from './../../base/Input/Input';
+import Question from './../../components/QuestionCard';
 import './../../style/base.css';
 import { useQuizLogic } from './useQuizLogic';
-const Quiz = () => {
 
+const Quiz = () => {
   const { id } = useParams();
   const { quiz, handleAnswerChange, handleSubmit } = useQuizLogic(id);
 
   return (
-    <>{!quiz ? <h1>Not a valid quiz id</h1> :
-      <div>
-        <h2>{quiz.title}</h2>
-        <h3>Questions:</h3>
+    <>
+      {!quiz ? (
+        <h1>Not a valid quiz id</h1>
+      ) : (
         <div>
-          {quiz.questions.map((question) => (
-            <div key={question.id}>
-              <p>{question.question}</p>
-
-              {question.type === "multiple-choice" ? (
-                <div>
-                  {question.options.map((option, index) => (
-                    <div key={index}>
-                      <input
-                        type="radio"
-                        name={question.id}
-                        onChange={() => handleAnswerChange(question.id, option)}
-                      />
-
-                      <label >{option}</label>
-                    </div>
-                  ))}
-                </div>
-              ) : (
-                <div>
-                  <Input placeholder="here" onChange={(e) => handleAnswerChange(question.id, e.target.value)} />
-                  <TextField id="outlined-basic" label="Outlined" variant="outlined" type="text"
-                    placeholder="Your answer"
-                    onChange={(e) => handleAnswerChange(question.id, e.target.value)} />
-
-                </div>
-              )}
-            </div>
-          ))}
+          <h2>{quiz.title}</h2>
+          <h3>Questions:</h3>
+          <div>
+            {quiz.questions.map((question) => (
+              <Question
+                key={question.id}
+                question={question}
+                handleAnswerChange={handleAnswerChange}
+              />
+            ))}
+          </div>
+          <ButtonComponent onClick={handleSubmit} text="Submit" />
         </div>
-
-        <ButtonComponent onClick={handleSubmit} text="submit" />
-      </div>}
+      )}
     </>
   );
+};
 
-
-
-}
 export default Quiz;
