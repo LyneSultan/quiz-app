5067eb38aa9569ceed035f6f9b684bf62e029045
LyneSultan <lynesultane@gmail.com>
2024-12-06 23:06:45 +0200

    created login and register apis

--- server-side/controllers/users.controller.js
+++ server-side/controllers/users.controller.js
@@ -1,21 +1,32 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const createUser = async (req, res) => {
+export const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    if (!username || !password) {
-      return res.status(500).send({
-        message: "All feilds are required",
+    const user = await User.findOne({
+      username,
+    });
+
+    if (!user) {
+      return res.status(404).send({
+        message: "Invalid Credentials",
       });
     }
 
-    const user = await User.create({
-      username,
-      password,
-    });
+    const check = await bcrypt.compare(password, user.password);
 
-    return res.json(user);
+    if (!check) {
+      return res.status(400).send({
+        message: "Invalid Credentials",
+      });
+    }
+
+    const token = await jwt.sign({ userId: user.id }, "secret");
+
+    return res.status(200).send({ user, token });
   } catch (error) {
     console.log(error.message);
 
@@ -25,3 +36,32 @@ export const createUser = async (req, res) => {
   }
 };
 
+export const register = async (req, res) => {
+  const { username, password, age } = req.body;
+
+  try {
+    if (!username || !password) {
+      return res.status(500).send({
+        message: "All feilds are required",
+      });
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      age,
+      password: hashed,
+    });
+    const token = await jwt.sign({ userId: user.id }, "secret");
+
+
+    return res.status(200).send({ user, token });
+  } catch (error) {
+    console.log(error.message);
+
+    return res.status(500).send({
+      message: "Something went wrong",
+    });
+  }
+};
