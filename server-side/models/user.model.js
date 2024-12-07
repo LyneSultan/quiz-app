720d73e2d0b8ad6dced3d532b5a4ac634c2fb828
LyneSultan <lynesultane@gmail.com>
2024-12-07 22:32:16 +0200

    created user model

--- server-side/models/user.model.js
+++ server-side/models/user.model.js
@@ -1,17 +1,16 @@
 import { Schema, model } from "mongoose";
 
-// create a schema => shapes and set rules for the inserted data
 const userSchema = new Schema({
   username: {
     type: String,
-    // required: true,
-    // unique: true,
+
   },
   password: {
     type: String,
-    // required: true,
+  },
+  score: {
+    type: Number,
   }
 });
 
-// create a model from the schema => resembles a collection in the db
 export const User = model("User", userSchema);
