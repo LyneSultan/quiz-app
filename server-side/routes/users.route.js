70437471f9caac81d10e37544089f3ea944a1090
LyneSultan <lynesultane@gmail.com>
2024-12-08 22:22:04 +0200

    created middleware

--- server-side/routes/users.route.js
+++ server-side/routes/users.route.js
@@ -4,13 +4,13 @@ import {
   login,
   register
 } from "../controllers/users.controller.js";
-
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = new Router();
 router.post("/register", register);
 router.post("/login", login);
 
 router.post("/createQuiz", createQuiz);
-router.get("/getQuizzes", getQuizzes);
+router.get("/getQuizzes", authMiddleware, getQuizzes);
 
 export default router;
