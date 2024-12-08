629177072e76585172263adf3d3f3c6fd8bb6085
LyneSultan <lynesultane@gmail.com>
2024-12-08 21:56:14 +0200

    edit quizzes page

--- app/src/pages/Quizzes/index.jsx
+++ app/src/pages/Quizzes/index.jsx
@@ -1,19 +1,13 @@
-import Card from '@mui/material/Card';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import QuizCard from '../../components/QuizCard.jsx';
-import { routes } from './../../routes/routes.js';
+import QuizCard from './../../components/QuizCard.jsx';
 import './../../style/base.css';
+import { useQuizzesLogic } from './useQuizzesLogic.js';
 
 const Quizzes = () => {
-  const quizzes = useSelector((state) => state.quizzes);
   const user = useSelector((state) => state.user);
-  const navigate = useNavigate();
 
-  const handleNavigate = (id) => {
-    navigate(routes.quiz(id));
-  };
-
+  const { quizzes, handleNavigate } = useQuizzesLogic();
   return (
 
     <div>
@@ -27,15 +21,17 @@ const Quizzes = () => {
         :
         <div >
           <div className='flex space-around'>
-            {quizzes.map((quiz) => (
-              <Card variant="outlined" key={quiz.id}>
+            {quizzes.map((quiz) => {
+              return (
                 <QuizCard quiz={quiz} handleNavigate={handleNavigate} />
-              </Card>
-            ))}
+              );
+            })}
+
           </div>
         </div>
       }
     </div >
   );
-}
+};
+
 export default Quizzes;
